8c48e8f1efd3c1f998f98960c3b116df00274f69
cuiqiguo <cuiqiguo@iauto.com>
2023-04-03 12:45:39 +0800

    add MOdel Explain File

--- test_main/main.cpp
+++ test_main/main.cpp
@@ -3,33 +3,12 @@
 
 using namespace std;
 
-int stone(int sts);
 
 int main()
 {
-    //int a = stone(2);
-    // cout << "==================hello C++ Learning========="
-    //      << "==================hello C++ Learning=========" << "======================"
-    //      << endl;
-    // shared_ptr<test_shared> ptr1(new test_shared("123"));
-    // shared_ptr<test_shared> ptr2;
-    // ptr2.reset(new test_shared("458"));
-    // shared_ptr<test_shared> ptr3;
-    // ptr3 = make_shared<test_shared>("568");
-    // int a = 30;
-    // int b = 20;
-    // int &c = a;
-    // Test_function Swap;
-    // Swap.Test_swap_one(a,b);
-    // cout << "Test a = "<< a << " Test b = " << b << endl;
-    // Swap.Test_swap_two(a,b);
-    // cout << "Test a = "<< a << " Test b = " << b << endl;
-    // Swap.Test_swap_three(&a,&b);
-    // cout << "Test a = " << a << " Test b = " << b << endl;
+    cout << "hello C++ Learning" << endl;
+    printf("hello C++ Learning\n");
     return 0;
 }
 
-int stone(int sts)
-{
-    return 14 * sts;
-}
+
--- toolchains/debug.toolchain.cmake
+++ toolchains/debug.toolchain.cmake
@@ -1,12 +1,13 @@
-# set cross-compiled system type, it's better not use the type which cmake cannot recognized.
-SET ( CMAKE_SYSTEM_NAME Linux )
-SET ( CMAKE_SYSTEM_PROCESSOR Linux )
-# when hislicon SDK was installed, toolchain was installed in the path as below: 
-SET ( CMAKE_C_COMPILER "/usr/bin/gcc" )
-SET ( CMAKE_CXX_COMPILER "/usr/bin/g++" )
-SET ( CMAKE_FIND_ROOT_PATH "/usr" )
 
-# set searching rules for cross-compiler
-SET ( CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER )
-SET ( CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY )
-SET ( CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY )
+SET ( CMAKE_SYSTEM_NAME Linux )        #指定系统名称
+SET ( CMAKE_SYSTEM_PROCESSOR Linux )   #指定处理器名称
+
+SET ( CMAKE_C_COMPILER "/usr/bin/gcc" )   #  C编译器路径
+SET ( CMAKE_CXX_COMPILER "/usr/bin/g++" ) #  C++编译器路径
+SET ( CMAKE_FIND_ROOT_PATH "/usr" )    #    指定了交叉编译环境路径(先搜索路径优先)，后搜索指定目录CMAKE_SYSTEM_ROOT路径下的文件
+
+SET ( CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER )  # 从来不在指定目录(交叉编译)下查找工具程序。(编译时利用的是宿主的工具)
+SET ( CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY )   # 只在指定的交叉编译路径下查找库文件
+SET ( CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY )   # 只在指定的交叉编译路径下查找头文件
+
+
